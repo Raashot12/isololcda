526ae472f679e1a9fd34ade6d76d24b5cd98c0f4
Raashot12 <akannirasheed1996@gmail.com>
2021-10-20 12:23:49 +0100

    Footer section completed

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,45 +1,44 @@
-import { Container } from '@material-ui/core'
-import React from 'react'
+import { Container } from "@material-ui/core"
+import React from "react"
 
 const ContactUs = () => {
-    return (
-        <div className="contact-us-contact">
-            <Container>
-                <div className="form-container">
-                    <form>
-
-                        <div className="input-container-form">
-                            <div>
-                                <label for="name">Name*</label>
-                            </div>
-                            <input type="text" placeholder="Enter your name" />
-                        </div>
-                        <div className="input-container-form">
-                            <div>
-                                <label for="email">Email*</label>
-                            </div>
-                            <input type="text" placeholder="Enter your email" />
-                        </div>
-                        <div className="input-container-form">
-                            <div>
-                                <label for="subject">Subject*</label>
-                            </div>
-                            <input type="text" placeholder="Type the subject" />
-                        </div>
-                        <div className="input-container-form">
-                            <div>
-                                <label for="message">Message</label>
-                            </div>
-                            <textarea type="text" placeholder="Enter your email" />
-                        </div>
-                        <div className="input-container-form">
-                            <button>Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </Container>
+  return (
+    <div className="contact-us-contact">
+      <Container>
+        <div className="form-container">
+          <form>
+            <div className="input-container-form">
+              <div>
+                <label for="name">Name*</label>
+              </div>
+              <input type="text" placeholder="Enter your name" />
+            </div>
+            <div className="input-container-form">
+              <div>
+                <label for="email">Email*</label>
+              </div>
+              <input type="text" placeholder="Enter your email" />
+            </div>
+            <div className="input-container-form">
+              <div>
+                <label for="subject">Subject*</label>
+              </div>
+              <input type="text" placeholder="Type the subject" />
+            </div>
+            <div className="input-container-form">
+              <div>
+                <label for="message">Message</label>
+              </div>
+              <textarea type="text" placeholder="Enter your email" />
+            </div>
+            <div className="input-container-form">
+              <button>Submit</button>
+            </div>
+          </form>
         </div>
-    )
+      </Container>
+    </div>
+  )
 }
 
 export default ContactUs
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react"
 
 const Footer = () => {
+    const getYear = new Date().getFullYear();
     return (
         <div>
-            ISOLO LOCAL COUNCIL DEVELOPMENT AREA
+            <div className="footer-first-item">
+                <h2>ISOLO LOCAL COUNCIL DEVELOPMENT AREA</h2>
+                <a
+                    href="mailto:olasojuadebayo.isololcda@gmail.com"
+                    className="partnership mailto"
+                    style={ { cursor: "pointer" } }
+                >
+                    olasojuadebayo.isololcda@gmail.com
+                </a>
+                <p className="telephone-number">123-456-7890</p>
+                <p className="address">Osolo Way, Aswani 102214, Lagos, Nigeria</p>
+            </div>
+            <div className="footerlast-item">
+                <h4>©{ getYear } by ISOLO LCDA. Proudly created with RashDev</h4>
+            </div>
         </div>
     )
 }
