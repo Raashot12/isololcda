38015b8df32cd3cbc168b806cc89f1ae73246c46
Raashot12 <akannirasheed1996@gmail.com>
2021-10-21 11:31:19 +0100

    menubar completed

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -9,25 +9,25 @@ const ContactUs = () => {
           <form>
             <div className="input-container-form">
               <div>
-                <label for="name">Name*</label>
+                <label htmlFor="name">Name*</label>
               </div>
               <input type="text" placeholder="Enter your name" />
             </div>
             <div className="input-container-form">
               <div>
-                <label for="email">Email*</label>
+                <label htmlFor="email">Email*</label>
               </div>
               <input type="text" placeholder="Enter your email" />
             </div>
             <div className="input-container-form">
               <div>
-                <label for="subject">Subject*</label>
+                <label htmlFor="subject">Subject*</label>
               </div>
               <input type="text" placeholder="Type the subject" />
             </div>
             <div className="input-container-form">
               <div>
-                <label for="message">Message</label>
+                <label htmlFor="message">Message</label>
               </div>
               <textarea type="text" placeholder="Enter your email" />
             </div>
--- src/components/TagMenu/index.jsx
+++ src/components/TagMenu/index.jsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { Container } from "@material-ui/core"
+import { navigate } from "@reach/router"
+
 export const NavbarMenuLinks = [
     {
         title: "Home",
@@ -49,51 +51,87 @@ const getSocialLinks = () => {
 }
 
 const TagMenu = () => {
+    const [hamburger, setHamburger] = useState( false )
+
+    const handleClose = () => {
+        setHamburger( !hamburger )
+    }
+    console.log( hamburger )
     return (
         <div className="tagline-banner">
             <Container>
-              <div className="tagline-container">
-                  <div className="hamburger-box">
-                      <i class="fas fa-hamburger">
-                          <span style={ { marginLeft: ".3rem" } }>Menu</span>
-                      </i>
-                  </div>
-                  <div className="menubar-list">
-                      { NavbarMenuLinks.map( navitem => (
-                          <Link to={ navitem.route } key={ navitem.route } className="nav-link">
-                              { navitem.title }
-                          </Link>
-            ) ) }
-                  </div>
-                  <div>
-                      { getSocialLinks().map( socialLink => (
-                          <Link
-                              target="_blank"
-                    to={ socialLink.href }
-                    key={ socialLink.id }
-                    className="social-mediai-icons"
-                    title={ socialLink.title }
-                >
-                    <span
-                        className={ `fab ${ socialLink.icon } icon-md-footer` }
-                        style={ { color: "white" } }
-                    ></span>
-                </Link>
-            ) ) }
-                  </div>
-              </div>
-          </Container>
-          <div></div>
-      </div>
-  )
+                <div className="tagline-container">
+                    <div className="hamburger-box">
+                        <i className="fas fa-hamburger" onClick={ handleClose }>
+                            <span style={ { marginLeft: ".3rem" } }>Menu</span>
+                        </i>
+                    </div>
+                    <div className="menubar-list">
+                        { NavbarMenuLinks.map( navitem => (
+                            <Link to={ navitem.route } key={ navitem.route } className="nav-link">
+                                { navitem.title }
+                            </Link>
+                        ) ) }
+                    </div>
+                    <div>
+                        { getSocialLinks().map( socialLink => (
+                            <a
+                                target="_blank"
+                                to={ socialLink.href }
+                                key={ socialLink.id }
+                                className="social-mediai-icons"
+                                title={ socialLink.title }
+                            >
+                                <span
+                                    className={ `fab ${ socialLink.icon } icon-md-footer` }
+                                    style={ { color: "white" } }
+                                ></span>
+                            </a>
+                        ) ) }
+                    </div>
+                </div>
+            </Container>
+            <MenuBar
+                handleClose={ handleClose }
+                setHamburger={ setHamburger }
+                hamburger={ hamburger }
+            />
+        </div>
+    )
 }
 
 export default TagMenu
 
-const MenuBar = () => {
+const MenuBar = ( { handleClose, setHamburger, hamburger } ) => {
+
+    const handleMenuClose = () => {
+        setHamburger( false )
+    }
     return (
         <div>
-            <div></div>
+            <div className={ `sidebar-container-${ hamburger }` }>
+                <i className="fas fa-times" onClick={ handleClose }></i>
+                { NavbarMenuLinks.map( navitem => (
+                    <Link to={ navitem.route } onClick={handleMenuClose } key={ navitem.route } className="sidebar-list">
+                        { navitem.title }
+                    </Link>
+                ) ) }
+                { getSocialLinks().map( socialLink => (
+                    <a
+                        target="_blank"
+                        to={ socialLink.href }
+                        key={ socialLink.id }
+                        className="social-mediai-icons"
+                        title={ socialLink.title }
+                    >
+                        <span
+                            onClick={ handleClose }
+                            className={ `fab ${ socialLink.icon } icon-md-footer-tagline` }
+                            style={ { color: "white" } }
+                        ></span>
+                    </a>
+                ) ) }
+            </div>
         </div>
     )
 }
