e4d6cff71416b676471988ac4d77bba9f4b91807
Raashot12 <akannirasheed1996@gmail.com>
2021-10-22 16:54:02 +0100

    how far

---
+++ src/helpers/LoaderSpinner/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Loader.css"
+const Loader = () => {
+    return (
+        <div class="loader">
+            <div class="loadingio-spinner-ripple-0r1rujj3n6i">
+                <div class="ldio-2skp4pzb2x5">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import ContactUs from "../components/ContactUs"
 import HeroSection from "../components/HeroSection"
 import Layout from "../components/layout"
-
-
+import Loader from "../helpers/LoaderSpinner/Loader"
 
 const IndexPage = () => {
+  const [spinner, setSpinner] = useState( true )
+
+  // It will be executed before rendering
+  useEffect( () => {
+    const id = setTimeout( () => setSpinner( false ), 1000 )
+
+     return () => {
+       clearTimeout( id );
+    };
+    }, [])
+
   return (
-    <Layout>
-      <HeroSection />
-      <ContactUs />
-    </Layout>
+    <>
+      { !spinner ? (
+        <Layout>
+          <HeroSection />
+          <ContactUs />
+        </Layout>
+      ) : (
+        <Loader />
+      ) }
+    </>
   )
 }
 
