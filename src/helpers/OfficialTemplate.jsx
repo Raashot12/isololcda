21ad64dcc185187b30f2d83c915f5bcd2cb23815
Raashot12 <akannirasheed1996@gmail.com>
2021-10-28 20:19:04 +0100

    develop

--- src/helpers/OfficialTemplate.jsx
+++ src/helpers/OfficialTemplate.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
-import { navigate } from '@reach/router'
+
 const OfficialTemplate = ( { title, pathimage, name } ) => {
     return (
         <div className="officials-card">
-            <img src={ pathimage } width="300px" loading="lazy" />
+            <img src={ pathimage } width="300px" loading="lazy" alt="council officials" />
             <div className="text-card">
                 <div>
                     <h4>{ name }</h4>
